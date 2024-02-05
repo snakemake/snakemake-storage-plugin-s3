c50d7c5b99acbf42e96f1aa2d717701ae2a15c27
Johannes Koester <johannes.koester@uni-due.de>
2024-02-05 21:28:57 +0100

    add workflow test

--- tests/tests.py
+++ tests/tests.py
@@ -3,8 +3,11 @@ import uuid
 from snakemake_interface_storage_plugins.tests import TestStorageBase
 from snakemake_interface_storage_plugins.storage_provider import StorageProviderBase
 from snakemake_interface_storage_plugins.settings import StorageProviderSettingsBase
+from snakemake_interface_executor_plugins.settings import ExecutorSettingsBase
 
 from snakemake_storage_plugin_s3 import StorageProvider, StorageProviderSettings
+import snakemake.common.tests
+import snakemake.settings
 
 
 class TestStorageNoSettings(TestStorageBase):
@@ -33,3 +36,22 @@ class TestStorageNoSettings(TestStorageBase):
 
     def get_example_args(self) -> List[str]:
         return []
+
+
+class TestWorkflows(snakemake.common.tests.TestWorkflowsMinioPlayStorageBase):
+    __test__ = True
+
+    def get_executor(self) -> str:
+        return "local"
+
+    def get_executor_settings(self) -> Optional[ExecutorSettingsBase]:
+        from snakemake.executors.local import ExecutorSettings
+        return ExecutorSettings()
+
+    def get_assume_shared_fs(self) -> bool:
+        return True
+
+    def get_remote_execution_settings(
+        self,
+    ) -> snakemake.settings.RemoteExecutionSettings:
+        return snakemake.settings.RemoteExecutionSettings()
