3eb308c7802016a29b29c04a16281a1fbd3b5d37
Johannes Koester <johannes.koester@uni-due.de>
2025-09-09 21:40:46 +0200

    dbg

--- snakemake_storage_plugin_s3/__init__.py
+++ snakemake_storage_plugin_s3/__init__.py
@@ -225,8 +225,8 @@ class StorageObject(StorageObjectRead, StorageObjectWrite, StorageObjectGlob):
             self.bucket = parsed.netloc
             # norm the path to avoid problems with double slashes and resolve ".."
             # (which is invalid for S3 keys).
-            #self.key = posixpath.normpath(parsed.path)
-            self.key = parsed.path.lstrip("/")
+            self.key = posixpath.normpath(parsed.path)
+            print(parsed.path.lstrip("/"), self.key, file=sys.stderr)
             self._local_suffix = self._local_suffix_from_key(self.key)
         self._is_dir = None
 
