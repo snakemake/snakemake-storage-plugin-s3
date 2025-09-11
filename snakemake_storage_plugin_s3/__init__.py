b40e72b1e487db8870f0904b7e6200b2756b60c4
Johannes Koester <johannes.koester@uni-due.de>
2025-09-11 07:50:11 +0200

    fix

--- snakemake_storage_plugin_s3/__init__.py
+++ snakemake_storage_plugin_s3/__init__.py
@@ -308,7 +308,7 @@ class StorageObject(StorageObjectRead, StorageObjectWrite, StorageObjectGlob):
     def size(self) -> int:
         # return the size in bytes
         if self.is_dir():
-            return sum(item.content_length for item in self.get_subkeys())
+            return sum(item.size for item in self.get_subkeys())
         else:
             return self.s3obj().content_length
 
